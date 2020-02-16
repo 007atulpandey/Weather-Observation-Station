667e1ccae891368fc21e60ad879f0ec2706b0bb2
007atulpandey <46052653+007atulpandey@users.noreply.github.com>
2020-02-16 15:08:01 +0530

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,1 +1,57 @@
+--- 1st problem 
+SELECT CITY,STATE FROM STATION;
 
+--- 2nd  problem 
+
+
+--- 3rd problem
+SELECT DISTINCT CITY FROM STATION 
+WHERE mod(ID,2) = 0;
+--- 4th problem 
+select count(city) - count(distinct city) from station ;
+
+--- 5th problem 
+select City, length from (select City, length(city)as length from STATION order by length asc, city asc)
+where rownum=1
+order by city;
+
+select City, length from (select City, length(city)as length from STATION order by length desc, city asc)
+where rownum=1
+order by city;
+
+--- 6 th problem 
+
+select CITY from station where CITY like 'A%' or CITY like 'E%' or CITY like 'I%' or CITY like 'O%' or CITY like 'U%' order by CITY;
+
+--- 7th problem 
+select distinct city from station 
+where substr(city,-1) in ('a','e','i','o','u');
+
+--- 8th problem 
+
+select distinct city from station 
+where lower(substr(city,1,1)) in ('a','e','i','o','u')
+and substr(city,-1) in ('a','e','i','o','u');
+                   
+ --- 9th problem 
+ 
+  select distinct city from station 
+where lower(substr(city,1,1)) not in ('a','e','i','o','u');
+--- 10th problem 
+select distinct city from station 
+where substr(city,-1) not in ('a','e','i','o','u');
+                   
+ --- 11th problem 
+                  select distinct city from station 
+where lower(substr(city,1,1)) not in ('a','e','i','u','o')
+or substr(city,-1) not in ('a','e','i','u','o');
+     
+                   
+                   
+--- 12 th problem ..
+                   
+select distinct city from station 
+where lower(substr(city,1,1)) not in ('a','e','i','u','o')
+and substr(city,-1) not in ('a','e','i','u','o');
+                   
+                   
